8c7dcda8ba8186537dcb99a4af36050ea62e01fb
Arnav Sharma <147113330+ArnavSharma938@users.noreply.github.com>
2024-05-12 12:17:18 -0400

    Add files via upload

---
+++ Dino.js
@@ -0,0 +1,193 @@
+//board
+let board;
+let boardWidth = 750;
+let boardHeight = 250;
+let context;
+
+//dino
+let dinoWidth = 88;
+let dinoHeight = 94;
+let dinoX = 50;
+let dinoY = boardHeight - dinoHeight;
+let dinoImg;
+
+let dino = {
+    x: dinoX,
+    y: dinoY,
+    width: dinoWidth,
+    height: dinoHeight
+}
+
+//cactus
+let cactusArray = [];
+
+let cactus1Width = 34;
+let cactus2Width = 69;
+let cactus3Width = 102;
+
+let cactusHeight = 70;
+let cactusX = 700;
+let cactusY = boardHeight - cactusHeight;
+
+let cactus1Img;
+let cactus2Img;
+let cactus3Img;
+
+//physics
+let velocityX = -8; //cactus moving left speed
+let velocityY = 0;
+let gravity = .4;
+let gameWon = false;
+let gameOver = false;
+let gamePaused = false; // New variable to control game pause
+let score = 0;
+
+window.onload = function () {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+
+    context = board.getContext("2d"); //used for drawing on the board
+
+    //draw initial dinosaur
+    // context.fillStyle="green";
+    // context.fillRect(dino.x, dino.y, dino.width, dino.height);
+
+    dinoImg = new Image();
+    dinoImg.src = "./img/dino.png";
+    dinoImg.onload = function () {
+        context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+    }
+
+    cactus1Img = new Image();
+    cactus1Img.src = "./img/cactus1.png";
+
+    cactus2Img = new Image();
+    cactus2Img.src = "./img/cactus2.png";
+
+    cactus3Img = new Image();
+    cactus3Img.src = "./img/cactus3.png";
+
+    requestAnimationFrame(update);
+    setInterval(placeCactus, 1000); //1000 milliseconds = 1 second
+    document.addEventListener("keydown", moveDino);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    if (gameOver || gameWon || gamePaused) { // Added gamePaused condition
+        return;
+    }
+    context.clearRect(0, 0, board.width, board.height);
+
+    //dino
+    velocityY += gravity;
+    dino.y = Math.min(dino.y + velocityY, dinoY); //apply gravity to current dino.y, making sure it doesn't exceed the ground
+    context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+
+    //cactus
+    for (let i = 0; i < cactusArray.length; i++) {
+        let cactus = cactusArray[i];
+        cactus.x += velocityX;
+        context.drawImage(cactus.img, cactus.x, cactus.y, cactus.width, cactus.height);
+
+        if (detectCollision(dino, cactus)) {
+            gameOver = true;
+            dinoImg.src = "./img/dino-dead.png";
+            dinoImg.onload = function () {
+                context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+            }
+        }
+    }
+
+    //score
+    context.fillStyle = "black";
+    context.font = "20px courier";
+    score++;
+    context.fillText(score, 5, 20);
+
+    // Check if the score reaches 1000
+    if (score >= 1000) {
+        gameWon = true;
+        // Add winning message
+        context.fillStyle = "green";
+        context.font = "80px Arial";
+        context.fillText("You Win!", boardWidth / 2 - 50, boardHeight / 2);
+        // Pause the game
+        gamePaused = true;
+        return;
+    }
+}
+
+function moveDino(e) {
+    if (gameOver || gameWon || gamePaused) { // Added gamePaused condition
+        return;
+    }
+
+    if ((e.code == "Space" || e.code == "ArrowUp") && dino.y == dinoY) {
+        //jump
+        velocityY = -8;
+    }
+    else if (e.code == "ArrowDown" && dino.y == dinoY) {
+        //duck
+    }
+}
+
+function placeCactus() {
+    if (gameOver || gameWon || gamePaused) { // Added gamePaused condition
+        return;
+    }
+
+    //place cactus
+    let cactus = {
+        img: null,
+        x: cactusX,
+        y: cactusY,
+        width: null,
+        height: cactusHeight
+    }
+
+    let placeCactusChance = Math.random(); //0 - 0.9999...
+
+    if (placeCactusChance > .70) { //10% you get cactus3
+        cactus.img = cactus3Img;
+        cactus.width = cactus3Width;
+        cactusArray.push(cactus);
+    }
+    else if (placeCactusChance > .50) { //30% you get cactus2
+        cactus.img = cactus2Img;
+        cactus.width = cactus2Width;
+        cactusArray.push(cactus);
+    }
+    else { //50% you get cactus1
+        cactus.img = cactus1Img;
+        cactus.width = cactus1Width;
+        cactusArray.push(cactus);
+    }
+
+    if (cactusArray.length > 5) {
+        cactusArray.shift(); //remove the first element from the array so that the array doesn't constantly grow
+    }
+}
+
+function detectCollision(a, b) {
+    // Define padding to reduce the hitbox size
+    let padding = 10; // Adjust this value to change the hitbox size
+
+    // Calculate the sides of the hitbox
+    let aLeft = a.x + padding;
+    let aRight = a.x + a.width - padding;
+    let aTop = a.y + padding;
+    let aBottom = a.y + a.height - padding;
+
+    let bLeft = b.x + padding;
+    let bRight = b.x + b.width - padding;
+    let bTop = b.y + padding;
+    let bBottom = b.y + b.height - padding;
+
+    // Check for collision
+    return !(aLeft > bRight ||
+        aRight < bLeft ||
+        aTop > bBottom ||
+        aBottom < bTop);
+}
